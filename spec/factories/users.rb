2b1446bf16d9163ee77d34c76318e4fb296ac267
hccadmin <dave_rothfarb@dfci.harvard.edu>
2016-03-15 17:59:40 -0400

    Added Factory and Faker fields for user model spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,10 @@
 
 FactoryGirl.define do
   factory :user do
-    firstname "MyString"
-    lastname "MyString"
-    email "MyString"
-    phone "MyString"
+    firstname { Faker::Name.first_name }
+    lastname { Faker::Name.last_name }
+    phone { Faker::PhoneNumber.phone_number }
+    password 'password'
+    password_confirmation 'password'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  it "is valid with username, firstname, lastname, phone, and email"
-  it "is invalid with a duplicate email address"
+  it "is valid with username, firstname, lastname, phone, and email" do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
+  it "is invalid with a duplicate username" do
+    user = create(:user, username: 'davemanroth')
+    expect(build(:user, username: 'davemanroth')).to be_invalid
+  end
+
+  it "is invalid with a duplicate email address" do
+    user = create(:user, email: 'dave@rothfarb.com')
+    expect(build(:user, email: 'dave@rothfarb.com')).to be_invalid
+  end
 end
