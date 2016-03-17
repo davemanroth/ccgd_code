29daac61f8583c39ed3750cc3a9f3260ee63354d
hccadmin <dave_rothfarb@dfci.harvard.edu>
2016-03-17 10:53:33 -0400

    Added all csv files, created state model, successfully seeded state with csv data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Seeding States data
+State.delete_all
+csv = File.join(Rails.root, 'app', 'csvs', 'states.csv')
+
+File.open(csv, 'r') do |file|
+  file.read.each_line do |state|
+    name, code = state.chomp.split(',')
+    State.create!(name: name, code: code)
+  end
+end
