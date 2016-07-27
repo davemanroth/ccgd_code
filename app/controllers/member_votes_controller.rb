70bebd50f1540a1c28583db00f1ef603ee6ba488
hccadmin <dave_rothfarb@dfci.harvard.edu>
2016-07-27 15:37:13 -0400

    Got the basics set up for the member votes admin view listing

--- app/controllers/member_votes_controller.rb
+++ app/controllers/member_votes_controller.rb
@@ -1,6 +1,12 @@
 class MemberVotesController < ApplicationController
   load_and_authorize_resource
 
+  def index
+    @proposal = Proposal.find(params[:proposal_id])
+    @committee = Committee.find(params[:committee_id])
+    @member_votes = MemberVote.where(committee_id: @committee)
+  end
+
   def new
     @proposal = Proposal.find(params[:proposal_id])
     @committee = Committee.find(params[:committee_id])
