ef45cfa660dcd1513d6db67d0217c143f71f7b25
hccadmin <dave_rothfarb@dfci.harvard.edu>
2016-08-15 15:32:05 -0400

    Added address list, new, and edit views. Edit button working on list view.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,22 +1,28 @@
 class AddressesController < ApplicationController
   def index
+    @addresses = Address.all
   end
 
   def show
+    @address = Address.find(params[:id])
   end
 
   def new
+    @address = Address.new
   end
 
   def create
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
   end
 
   def destroy
+    @address = Address.find(params[:id])
   end
 end
