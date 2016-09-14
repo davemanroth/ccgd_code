45fc2e1d85924582b701c5085ecdb09d3981ce37
hccadmin <dave_rothfarb@dfci.harvard.edu>
2016-09-14 12:53:52 -0400

    Changed admin email address to ccgd one, added myself on bcc

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,14 +1,14 @@
 class AdminMailer < ApplicationMailer
 
-  default to: 'dave_rothfarb@dfci.harvard.edu'
+  default to: 'ccgd@partners.org'
 
   def new_user(user)
     @user = user
-    mail(subject: 'New user signup')
+    mail(subject: 'New user signup', bcc: ['dave_rothfarb@dfci.harvard.edu'])
   end
 
   def new_proposal(prop)
     @proposal = prop
-    mail(subject: 'New proposal submitted')
+    mail(subject: 'New proposal submitted', bcc: ['dave_rothfarb@dfci.harvard.edu'])
   end
 end
