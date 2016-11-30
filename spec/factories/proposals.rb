4ac806759582dfde6115d0b0476e6e6761f0c255
David Rothfarb <david@dr809.dfci.harvard.edu>
2016-11-30 10:34:30 -0500

    Take 3 - fix proposal list: installed site locally and was able to troubleshoot. Needed to replace check for nil with check for empty on both lagroup and platforms within proposal list view

--- spec/factories/proposals.rb
+++ spec/factories/proposals.rb
@@ -13,7 +13,7 @@ FactoryGirl.define do
     proposal_status_id 1
     lab_group_id 27
     sample_types [SampleType.find(1)]
-    platforms [Platform.find(1), Platform.find(2)]
+    platforms [Platform.new, Platform.new]
 
     after(:build) do |prop|
       prop.user = FactoryGirl.create(:user)
